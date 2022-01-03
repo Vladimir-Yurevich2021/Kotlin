4e982666a2f8c162f9792f93aea5d68944f18d70
Vladimir_Yurevich <dvagig@gmail.com>
2022-01-03 15:15:33 +0300

    Нашли кнопку в активити и повесили на неё слушатель

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import com.example.myapplication.R
 
@@ -9,6 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+val button=findViewById<Button>(R.id.btn_click_me)
+        button.setOnClickListener(View.OnClickListener { button.setText(getString(R.string.txt_great)) })
     }
 }
